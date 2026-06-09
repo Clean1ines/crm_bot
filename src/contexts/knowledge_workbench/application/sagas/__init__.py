d39051ec350ba1f96e4450ddf0e4b43682492ef2
haku <100520276+Clean1ines@users.noreply.github.com>
2026-06-09 23:15:52 +0300

    Export draft observation extraction work index port

--- src/contexts/knowledge_workbench/application/sagas/__init__.py
+++ src/contexts/knowledge_workbench/application/sagas/__init__.py
@@ -1,30 +1,10 @@
-from .knowledge_extraction_saga import (
-    KnowledgeExtractionSaga,
-    ReconcileKnowledgeExtractionSagaCommand,
-    ReconcileKnowledgeExtractionSagaResult,
-)
-from .knowledge_extraction_saga_ports import (
-    KnowledgeExtractionCommandEmitterPort,
-    KnowledgeExtractionCommandLogPort,
-    KnowledgeExtractionCommandRecord,
-    KnowledgeExtractionEventCursorPort,
-    KnowledgeExtractionEventCursorRecord,
-    KnowledgeExtractionSagaStateRepositoryPort,
-)
-from .knowledge_extraction_saga_state import (
-    KnowledgeExtractionPhaseCheckpoint,
-    KnowledgeExtractionPhaseKey,
-    KnowledgeExtractionPhaseStatus,
-    KnowledgeExtractionWorkflowState,
-    KnowledgeExtractionWorkflowStatus,
-)
-from .knowledge_extraction_source_phase_reconciliation import (
-    KnowledgeExtractionSourcePhaseReconciler,
-    SourcePhaseReconciliationResult,
-    SourcePhaseReconciliationStatus,
-)
+from .knowledge_extraction_saga import KnowledgeExtractionSaga, ReconcileKnowledgeExtractionSagaCommand, ReconcileKnowledgeExtractionSagaResult
+from .knowledge_extraction_saga_ports import DraftObservationExtractionWorkIndexPort, KnowledgeExtractionCommandEmitterPort, KnowledgeExtractionCommandLogPort, KnowledgeExtractionCommandRecord, KnowledgeExtractionEventCursorPort, KnowledgeExtractionEventCursorRecord, KnowledgeExtractionSagaStateRepositoryPort
+from .knowledge_extraction_saga_state import KnowledgeExtractionPhaseCheckpoint, KnowledgeExtractionPhaseKey, KnowledgeExtractionPhaseStatus, KnowledgeExtractionWorkflowState, KnowledgeExtractionWorkflowStatus
+from .knowledge_extraction_source_phase_reconciliation import KnowledgeExtractionSourcePhaseReconciler, SourcePhaseReconciliationResult, SourcePhaseReconciliationStatus
 
 __all__ = (
+    "DraftObservationExtractionWorkIndexPort",
     "KnowledgeExtractionCommandEmitterPort",
     "KnowledgeExtractionCommandLogPort",
     "KnowledgeExtractionCommandRecord",
