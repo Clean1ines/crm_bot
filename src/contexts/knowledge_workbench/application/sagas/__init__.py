c65623c3b82e4a62f7af0fb44187e9ecee29c05b
haku <100520276+Clean1ines@users.noreply.github.com>
2026-06-09 20:38:43 +0300

    Export knowledge extraction saga contracts

---
+++ src/contexts/knowledge_workbench/application/sagas/__init__.py
@@ -0,0 +1,37 @@
+from .knowledge_extraction_saga import (
+    KnowledgeExtractionSaga,
+    ReconcileKnowledgeExtractionSagaCommand,
+    ReconcileKnowledgeExtractionSagaResult,
+)
+from .knowledge_extraction_saga_ports import (
+    KnowledgeExtractionCommandEmitterPort,
+    KnowledgeExtractionCommandLogPort,
+    KnowledgeExtractionCommandRecord,
+    KnowledgeExtractionEventCursorPort,
+    KnowledgeExtractionEventCursorRecord,
+    KnowledgeExtractionSagaStateRepositoryPort,
+)
+from .knowledge_extraction_saga_state import (
+    KnowledgeExtractionPhaseCheckpoint,
+    KnowledgeExtractionPhaseKey,
+    KnowledgeExtractionPhaseStatus,
+    KnowledgeExtractionWorkflowState,
+    KnowledgeExtractionWorkflowStatus,
+)
+
+__all__ = (
+    "KnowledgeExtractionCommandEmitterPort",
+    "KnowledgeExtractionCommandLogPort",
+    "KnowledgeExtractionCommandRecord",
+    "KnowledgeExtractionEventCursorPort",
+    "KnowledgeExtractionEventCursorRecord",
+    "KnowledgeExtractionPhaseCheckpoint",
+    "KnowledgeExtractionPhaseKey",
+    "KnowledgeExtractionPhaseStatus",
+    "KnowledgeExtractionSaga",
+    "KnowledgeExtractionSagaStateRepositoryPort",
+    "KnowledgeExtractionWorkflowState",
+    "KnowledgeExtractionWorkflowStatus",
+    "ReconcileKnowledgeExtractionSagaCommand",
+    "ReconcileKnowledgeExtractionSagaResult",
+)
